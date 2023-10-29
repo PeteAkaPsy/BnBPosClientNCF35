67d5cfcc14edff3c9d6c53ef0e1e04da538ef4c2
Peter Renkewitz <28795835+PeteAkaPsy@users.noreply.github.com>
2023-10-29 22:53:20 +0100

    added the rest api and a test project for the created api's

---
+++ RestTestCF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestTestCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
