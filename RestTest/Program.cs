3674a7edc52407323122e036f0732b3dda536003
Peter Renkewitz <28795835+PeteAkaPsy@users.noreply.github.com>
2024-02-17 16:42:13 +0100

    added testproject running from pc cmd instead cf and added mini test for login - now after fixing restcf and testing, the basic rest functions work

---
+++ RestTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RestTest
+{
+    class Program
+    {
+
+        bool hasRunLogin = false;
+        bool hasRunTest = false;
+        bool hasRunLogout = false;
+
+        static void Main(string[] args)
+        {
+            string testIP = "http://192.168.178.67:8081/";
+
+            RetroLab.REST.RestClient restClient = new RetroLab.REST.RestClient(testIP);
+
+            LoginData loginData;
+            loginData.userName = "Admin";
+            loginData.passwd = "Admin";
+
+            restClient.RequestAuth("r/login", loginData, 
+                result => {
+                    Console.WriteLine("LoginResult: " + result);
+                    restClient.Get<bool>("r/testlogin", null, 
+                        result2 => { 
+                            Console.WriteLine("Authenticated: " + result2);
+                            restClient.DiscardAuth("r/logout");
+                        }, errors => { });
+                }, errors => { });
+
+            while (true)
+            {
+            }
+        }
+
+        [Serializable]
+        public struct LoginData
+        {
+            public string userName;
+            public string passwd;
+        }
+    }
+}
